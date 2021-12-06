57b0cefea1cdd9b5aa1a850c425758174d5da2a3
Shay Narang <shaynarang@admins-MBP-3.hsd1.tn.comcast.net>
2021-12-06 00:23:24 -0600

    add run file

---
+++ run.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Dir['./lib/*.rb'].each { |file| require file }
+
+puts StringColorer.color('cyan', Message.welcome_prompt)
+
+loop do
+  time = gets.chomp
+  break if time == 'q'
+
+  if Converter.time_valid?(time)
+    converter = Converter.new(time)
+    english = converter.convert
+    puts StringColorer.color('green', english)
+    puts StringColorer.color('cyan', Message.subsequent_prompt)
+  else
+    puts StringColorer.color('red', Message.time_error)
+  end
+end
